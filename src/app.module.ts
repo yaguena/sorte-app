5df9958ef02b6df9cd266c83ba15a8379015f64b
Gabriel Aguena <yoshikichi@gmail.com>
2023-09-11 20:59:16 -0300

    first

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot()
+  ],
+})
+export class AppModule {}
